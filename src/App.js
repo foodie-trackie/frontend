d63d76f3ed01bca6900b2c40b5a2d40ec61b4f9e
Ziyu Chen <zc839@nyu.edu>
2020-01-21 16:54:08 +0800

    Finished the sign in page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Signin from "./components/Signin";
-import AddItem from "./components/AddItem";
+import Signin from "./components/Signin";
+// import AddItem from "./components/AddItem";
 // import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <AddItem />
+      <Signin />
     </div>
   );
 }
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import "./Signin.css";
 import axios from "axios";
 import Cookies from "js-cookie";
+import _ from "lodash";
+
 // import { connect } from "react-redux";
 
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -18,23 +21,51 @@ class Signin extends React.Component {
       name: "",
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      succeeded: false,
+      failed: false,
+      errors: []
     };
   }
 
+  handleClose = () => {
+    this.setState({ succeeded: false, failed: false });
+  };
+
   handleSubmit = () => {
     const csrftoken = Cookies.get("csrftoken"); // Using JS Cookies library
-    const headers = { X_CSRFTOKEN: csrftoken };
+    const headers = {
+      X_CSRFTOKEN: csrftoken
+      // "Content-Type": "application/x-www-form-urlencoded"
+      // "Content-Type": "application/json"
+    };
     console.log(headers);
-    axios.post(
-      "api/signup/",
-      {
-        username: this.state.name,
-        email: this.state.email,
-        password: this.state.password
-      },
-      { headers }
-    );
+    axios
+      .post(
+        "api/signup/",
+        {
+          username: this.state.name.replace(" ", "_"),
+          email: this.state.email,
+          password: this.state.password,
+          password1: this.state.password,
+          password2: this.state.confirmPassword
+        },
+        { headers }
+      )
+      .then(() => {
+        this.setState({ succeeded: true });
+      })
+      .catch(error => {
+        const errors = _.flatten(Object.values(error.response.data)).map(
+          item => item.message
+        );
+
+        console.log(errors);
+        this.setState({
+          failed: true,
+          errors
+        });
+      });
   };
 
   render() {
@@ -134,6 +165,30 @@ class Signin extends React.Component {
               <Button variant="secondary">Sign In</Button>
             )}
           </Form>
+          <Modal show={this.state.succeeded} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Sign up successfully</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Congratulations! You have successfully created an account!
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+          <Modal show={this.state.failed} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Error Message(s)</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {this.state.errors.map((error, key) => (
+                <div key={key}>{error}</div>
+              ))}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
     );
