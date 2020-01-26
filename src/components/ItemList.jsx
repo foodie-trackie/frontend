ac077f4e9436d04e1b0c1ee7c22786b4ad73dd56
Ziyu Chen <zc839@nyu.edu>
2020-01-26 22:22:09 +0800

    Added a filter to ItemList component.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Form from "react-bootstrap/Form";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { connect } from "react-redux";
@@ -13,28 +12,67 @@ class ItemList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: "all"
+      limit: Number.POSITIVE_INFINITY
     };
   }
+  handleChange = e => {
+    this.setState({
+      limit: e.target.value
+    });
+  };
+
   render() {
+    const displayedItems = this.props.items.filter(
+      item => dateToDuration(item.expirationDate) <= Number(this.state.limit)
+    );
     return (
       <div className="item-list">
-        {this.props.items.map((item, key) => (
-          <Card bg={dateToColor(item.expirationDate)} key={key}>
-            <Card.Header>
-              Expires {moment(item.expirationDate).fromNow()}{" "}
-            </Card.Header>
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Text>{item.number} left</Card.Text>
-            </Card.Body>
-            {/* <ListGroup>
+        <Form>
+          <Form.Group controlId={1}>
+            <Form.Label>What items do you want to display?</Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.limit}
+              onChange={this.handleChange}
+            >
+              <option value={Number.POSITIVE_INFINITY}>All items</option>
+              <option value={0}>Items that are expiring today</option>
+              <option value={1}>Items that are expiring tomorrow</option>
+              <option value={3}>Items that are expiring in three days</option>
+              <option value={7}>Items that are expiring in a week</option>
+              <option value={30}>Items that are expiring in a month</option>
+              <option value={365}>Items that are expiring in a year</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        {displayedItems.length ? (
+          displayedItems.map((item, key) => (
+            <div key={key}>
+              <br />
+              <Card
+                bg={dateToColor(item.expirationDate)}
+                style={{ width: 300 }}
+              >
+                <Card.Header>
+                  Expires {moment(item.expirationDate).fromNow()}{" "}
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>{item.number} left</Card.Text>
+                </Card.Body>
+                {/* <ListGroup>
               <ListGroupItem>{item.name}</ListGroupItem>
               <ListGroupItem>{item.number} left</ListGroupItem>
               <ListGroupItem></ListGroupItem>
             </ListGroup> */}
-          </Card>
-        ))}
+              </Card>
+
+              <br />
+            </div>
+          ))
+        ) : (
+          <div>There is no such item.</div>
+        )}
       </div>
     );
   }
@@ -43,6 +81,9 @@ class ItemList extends React.Component {
 const mapStateToProps = state => ({
   items: state.items
 });
+
+const dateToDuration = date =>
+  moment.duration(moment(date).diff(moment())).days();
 
 const dateToColor = date => {
   const duration = moment.duration(moment(date).diff(moment())).days();
